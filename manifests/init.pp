e4329e884e0d4a0c34bcd61c5f7ba2d35514fecb
tavizon <iktome@adohi.net>
2014-06-30 19:58:35 -0700

    Updated README.md to reflect lighttable

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # 
 class lighttable {
   package { 'LightTable':
-    provider => 'compressed_app',
     source   => 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.6/LightTableMac.zip',
+    provider => 'compressed_app'
   }
 }
