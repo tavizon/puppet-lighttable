8d52f5d4ce3345dde56ac013275fe8eb1f2fd3f2
tavizon <iktome@adohi.net>
2014-06-30 20:10:04 -0700

    Added version variable for updating and parameter passing.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
-# 
-class lighttable {
+#
+class lighttable ($version = "0.6.6") {
   package { 'LightTable':
-    source   => 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.6/LightTableMac.zip',
+    source   => "https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/${version}/LightTableMac.zip",
     provider => 'compressed_app'
   }
 }
