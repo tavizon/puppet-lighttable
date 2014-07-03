afd4891156c13f6da94623532dd8be382e1e633c
Jesse Tavizón <iktome@adohi.net>
2014-07-03 15:36:03 -0700

    Update lighttable_spec.rb

--- spec/classes/lighttable_spec.rb
+++ spec/classes/lighttable_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 # or read http://rspec-puppet.com/tutorial/
 describe 'lighttable' do
   it do
+    should contain_class('lighttable')
     should contain_package('LightTable').with({
       :source   => 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.6/LightTableMac.zip',
       :provider => 'compressed_app'
